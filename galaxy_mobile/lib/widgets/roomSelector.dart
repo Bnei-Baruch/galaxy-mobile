81ebf492c04b088e138d101c7dd149e84d341f7b
igal <igal38@gmail.com>
2021-12-02 04:24:23 +0200

    fixing hebrew ltr string search

--- galaxy_mobile/lib/widgets/roomSelector.dart
+++ galaxy_mobile/lib/widgets/roomSelector.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:galaxy_mobile/models/mainStore.dart';
@@ -49,11 +51,55 @@ class RoomSelector extends StatelessWidget {
       ),
       showClearButton: true,
       showSearchBox: true,
+      autoFocusSearchBox: true,
+      filterFn:(room,string){
+
+        if(string.isEmpty)
+          return true;
+
+        if(Bidi.hasAnyLtr(string))
+          {
+            List<int> newList = List.from(string.codeUnits);
+            newList.removeWhere((element) =>
+            element == Bidi.LRE.codeUnits.first || element == Bidi.LRM.codeUnits.first || element == Bidi.PDF.codeUnits.first
+            );
+            String newString = String.fromCharCodes(newList);return room.description.toLowerCase().contains(newString);
+          }
+        else
+          {
+            return room.description.toLowerCase().contains(string);
+          }
+      } ,
       searchBoxDecoration: InputDecoration(
         border: OutlineInputBorder(),
         contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
         labelText: "Search",
       ),
     );
+  }
+  String replaceNoPrintable(String value, {String replaceWith = ' '}) {
+    var charCodes = <int>[];
+
+    for (final codeUnit in value.codeUnits) {
+      if (isPrintable(codeUnit)) {
+        charCodes.add(codeUnit);
+      } else {
+        if (replaceWith!= null && replaceWith.isNotEmpty) {
+          charCodes.add(replaceWith.codeUnits[0]);
+        }
+
+      }
+    }
+
+    return String.fromCharCodes(charCodes);
+  }
+
+  bool isPrintable(int codeUnit) {
+    var printable = true;
+
+    if (codeUnit < 33) printable = false;
+    if (codeUnit >= 127) printable = false;
+
+    return printable;
   }
 }
