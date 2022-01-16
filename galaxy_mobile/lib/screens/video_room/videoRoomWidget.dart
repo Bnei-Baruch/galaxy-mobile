9f0ab842773a40e7aafd00e377100030f61fb4f1
igal <igal38@gmail.com>
2022-01-16 03:50:53 +0200

    remove unused code

--- galaxy_mobile/lib/screens/video_room/videoRoomWidget.dart
+++ galaxy_mobile/lib/screens/video_room/videoRoomWidget.dart
@@ -1781,37 +1781,7 @@ class _VideoRoomState extends State<VideoRoom> with WidgetsBindingObserver {
     setState(() {});
   }
 
-  initSelfVideo() async {
-    widget._localRenderer = new RTCVideoRenderer();
-    await widget._localRenderer.initialize();
-
-
-    var mediaConstraints = {
-      "audio": false,
-      "video": {
-        "mandatory": {
-          "minWidth":
-          '1280', // Provide your own width, height and frame rate here
-          "minHeight": '720',
-          "minFrameRate": '30',
-        },
-        "facingMode": "user",
-        "optional": [],
-      }
-    };
-    MediaStream stream =
-    await navigator.mediaDevices.getUserMedia(mediaConstraints);
-
-     setState(() {
-      widget.myStream = stream;
-      stream.getVideoTracks().first.enabled = true;
-      widget._localRenderer.srcObject = stream;
-     });
-
-
-
-    cameras = await Helper.cameras;
-  }
+ 
 
   void restartSelfVideo() async {
 
