d5f74f6734d02aef8a4ea0166e713b58cb29f063
igal <igal38@gmail.com>
2024-04-11 04:13:35 +0300

    RC 1.3.3 BLUETOOTH PERMISSIONS FOR ANDROID 12 AND UP

--- galaxy_mobile/lib/screens/login/login.dart
+++ galaxy_mobile/lib/screens/login/login.dart
@@ -14,6 +14,7 @@ import 'package:galaxy_mobile/widgets/ui_language_selector.dart';
 import 'package:new_version/new_version.dart';
 import 'package:package_info/package_info.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -176,6 +177,11 @@ class _LoginState extends State<Login>  with WidgetsBindingObserver {
         });
     } else {
       store.setLastLogin(DateTime.now());
+
+      Map<Permission, PermissionStatus> statuses = await [
+        Permission.bluetooth,
+      ].request();
+      print(statuses[Permission.bluetooth]);
       Navigator.pushNamed(context, '/settings');
     }
   }
